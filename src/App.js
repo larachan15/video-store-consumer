ac2cc5990c3bfcfc3ae2fc1fe1b1bbce98f878b5
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-12-17 17:36:40 -0800

    add library, search, and customer links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import Customer from './components/Customer';
+import Library from './components/Library';
+import Search from './components/Search';
 
 import {
   BrowserRouter as Router,
@@ -19,10 +21,14 @@ class App extends Component {
         <div className="App">
           <div className="container">
             <ul>
-              <li><a href="">Customers</a></li>
+              <li><Link to="/customer">Customers</Link></li>
+              <li><Link to="/library">Library</Link></li>
+              <li><Link to="/search">Search</Link></li>
             </ul>
             {/* Routes will go here */}
             <Route path="/customer" component={Customer} />
+            <Route path="/library" component={Library} />
+            <Route path="/search" component={Search} />
           </div>
         </div>
       </Router>
