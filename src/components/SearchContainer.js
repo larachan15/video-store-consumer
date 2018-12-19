51a1f4ae0e9b8f57a2d1b0233369ca3b1bdafa7b
Dionisia Montanez <dmontanez15@gmail.com>
2018-12-19 14:47:47 -0800

    can add movie to movie library

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -8,6 +8,8 @@ import Movie from './Movie';
 
 const SEARCH_URL = "http://localhost:3000/movies?query="
 
+const ADD_URL = "http://localhost:3000/movies?"
+
 class SearchContainer extends Component {
   constructor () {
     super();
@@ -18,7 +20,6 @@ class SearchContainer extends Component {
   }
 
   searchMovies = (title) => {
-
 
     axios.get(SEARCH_URL + title)
     .then((response) => {
@@ -33,14 +34,36 @@ class SearchContainer extends Component {
     });
   }
 
+  addMovieToLibrary = (movie) => {
+    console.log(movie.title)
+    console.log(movie.image_url)
+    const url = ADD_URL + `title=${movie.title}&overview=${movie.overview}&release_date=${movie.release_date}&image_url=${movie.image_url}&external_id=${movie.external_id}`
+    console.log("Movie is being added");
+    console.log(url);
+
+    axios.post(url)
+    .then((response) => {
+      console.log(response.data);
+      // this.setState({
+      //   movies: response.data
+      // })
+    })
+    .catch((error) => {
+      console.log('failure response');
+      console.log(error);
+    });
+  }
+
+
   render() {
-    const movies = this.state.movies
-    const moviesList = movies.map((movie) => {
+    // const movies = this.state.movies;
+    const moviesList = this.state.movies.map((movie) => {
       return <Movie
         key={movie.id}
         title={movie.title}
         image={movie.image_url}
         release={movie.release_date}
+        onMovieSelected={() => this.addMovieToLibrary(movie)}
         />
     });
 
